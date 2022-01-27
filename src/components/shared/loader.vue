0c7fb67779e6e3a30ad8f63736fc37ff627f4ffc
javierdel <ashair455@gmail.com>
2022-01-26 23:25:27 -0400

    loader Component

---
+++ src/components/shared/loader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="justify-center"
+    :class="{'fullscreen': visible}"
+  >
+      <q-inner-loading :showing="visible"
+        label="Cargando..."
+        label-class="text-normal"
+        label-style="font-size: 1.1em"
+        size="150px"
+      >
+      </q-inner-loading>
+  </div>
+</template>
+<script setup>
+  import { computed } from 'vue';
+  import store from '../../store';
+  const visible = computed( () => store.state.main.loading )
+</script>
