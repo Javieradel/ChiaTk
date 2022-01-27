b87b4ab3f6c309dc58cbad644e2ceaa1f16a6afa
javierdel <ashair455@gmail.com>
2022-01-26 23:26:46 -0400

    modal component add home

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,7 +1,13 @@
 <template>
-    <h1>HOME</h1>
-    <CarouselVue></CarouselVue>
+  <h1>HOME</h1>
+  <CarouselVue></CarouselVue>
+  <modalVue></modalVue>
 </template>
 <script setup>
-    import CarouselVue from './shared/Carousel.vue';
+import CarouselVue from './shared/Carousel.vue'
+import modalVue from './shared/modal.vue';
+import store from '../store';
+function showModal(){
+  //store.commit('main/SHOW_MODAL', { show:true, data:{title:'test',type:'info', message:'esta es una prueba'} })
+}
 </script>
