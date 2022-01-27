d15144da83ceb89e8fdd577bc5afdad2e841bbb5
javierdel <ashair455@gmail.com>
2022-01-26 23:27:21 -0400

    route login

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,23 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import PublicView from '/src/components/layouts/PublicView.vue'
-
+import Login from '/src/components/layouts/Login.vue'
 const routes = [
   {
     path: '/',
     component: PublicView,
+    // children: [
+    //   {
+    //     path: '/',
+    //     name: 'Home',
+    //     component: HelloWorld
+    //   },
+      // NEW ROUTES
+      //...routesOrganizations,
+   // ]
+  },
+  {
+    path: '/login',
+    component: Login,
     // children: [
     //   {
     //     path: '/',
